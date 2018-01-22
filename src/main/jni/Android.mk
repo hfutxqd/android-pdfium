d9075d229676c8073ef530a015a1a705d0dfa19b
Alexey Kuznetsov <axet@me.com>
2018-01-22 14:29:33 +0300

    rebuild pdfium

--- src/main/jni/Android.mk
+++ src/main/jni/Android.mk
@@ -1,45 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
-#Prebuilt libraries
 include $(CLEAR_VARS)
-LOCAL_MODULE := aospPdfium
+LOCAL_MODULE := pdfium
 
 ARCH_PATH = $(TARGET_ARCH_ABI)
+
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
     ARCH_PATH = armeabi
 endif
 
-ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
-    ARCH_PATH = arm64
-endif
-
-LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/$(ARCH_PATH)/libmodpdfium.so
+LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/$(ARCH_PATH)/libpdfium.so
 
 include $(PREBUILT_SHARED_LIBRARY)
 
-#libmodft2
 include $(CLEAR_VARS)
-LOCAL_MODULE := libmodft2
-
-LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/$(ARCH_PATH)/libmodft2.so
-
-include $(PREBUILT_SHARED_LIBRARY)
-
-#libmodpng
-include $(CLEAR_VARS)
-LOCAL_MODULE := libmodpng
-
-LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/$(ARCH_PATH)/libmodpng.so
-
-include $(PREBUILT_SHARED_LIBRARY)
-
-#Main JNI library
-include $(CLEAR_VARS)
-LOCAL_MODULE := jniPdfium
+LOCAL_MODULE := pdfiumjni
 
 LOCAL_CFLAGS += -DHAVE_PTHREADS
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
-LOCAL_SHARED_LIBRARIES += aospPdfium libmodft2 libmodpng
+LOCAL_SHARED_LIBRARIES += pdfium
 LOCAL_LDLIBS += -llog -landroid -ljnigraphics
 
 LOCAL_SRC_FILES :=  $(LOCAL_PATH)/src/mainJNILib.cpp
--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,9 +4,4 @@ APP_CPPFLAGS += -fexceptions
 #For ANativeWindow support
 APP_PLATFORM = android-9
 
-APP_ABI :=  armeabi \
-            armeabi-v7a \
-            arm64-v8a \
-            mips \
-            x86 \
-            x86_64
+APP_ABI :=  armeabi armeabi-v7a arm64-v8a mips mips64 x86 x86_64
